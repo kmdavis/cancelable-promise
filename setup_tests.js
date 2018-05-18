ce0de149b670f16d9d420ad0c7d3f59c11a2503f
Kevan Davis <kevan.davis@handshake.com>
2018-05-18 11:03:11 -0500

    removing some commented out lines

--- setup_tests.js
+++ setup_tests.js
@@ -1,12 +1,10 @@
 import "babel-polyfill";
 import chai, { expect } from "chai"; // assertions
-// import chaiAsPromised from "chai-as-promised";
 import sinon from "sinon";
 import sinonChai from "sinon-chai"; // assertions for sinon
 import sinonTest from "sinon-test";
 
 chai.use(sinonChai);
-// chai.use(chaiAsPromised);
 global.expect = expect;
 
 global.sinon = sinon;
