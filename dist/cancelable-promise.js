930ade22a83b5a046c1044460b3cf9c76adac2d9
Kevan Davis <kevan.davis@handshake.com>
2018-05-21 09:19:59 -0500

    more inclusive target list (default)

--- dist/cancelable-promise.js
+++ dist/cancelable-promise.js
@@ -16,14 +16,43 @@
     Object.defineProperty(exports, "__esModule", {
         value: true
     });
-    const IS_CANCELED = Symbol("isCanceled");
-    const IS_REJECTED = Symbol("isRejected");
-    const IS_RESOLVED = Symbol("isResolved");
-    const ON_CANCEL = Symbol("onCancel");
-    const INTERNAL_PROMISE = Symbol("internalPromise");
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var _createClass = function () {
+        function defineProperties(target, props) {
+            for (var i = 0; i < props.length; i++) {
+                var descriptor = props[i];
+                descriptor.enumerable = descriptor.enumerable || false;
+                descriptor.configurable = true;
+                if ("value" in descriptor) descriptor.writable = true;
+                Object.defineProperty(target, descriptor.key, descriptor);
+            }
+        }
+
+        return function (Constructor, protoProps, staticProps) {
+            if (protoProps) defineProperties(Constructor.prototype, protoProps);
+            if (staticProps) defineProperties(Constructor, staticProps);
+            return Constructor;
+        };
+    }();
+
+    var IS_CANCELED = Symbol("isCanceled");
+    var IS_REJECTED = Symbol("isRejected");
+    var IS_RESOLVED = Symbol("isResolved");
+    var ON_CANCEL = Symbol("onCancel");
+    var INTERNAL_PROMISE = Symbol("internalPromise");
 
     function fnWrapper(fn) {
-        return fn ? (...args) => this[IS_CANCELED] ? undefined : fn(...args) : undefined;
+        var _this = this;
+
+        return fn ? function () {
+            return _this[IS_CANCELED] ? undefined : fn.apply(undefined, arguments);
+        } : undefined;
     }
 
     /**
@@ -41,8 +70,13 @@
      * p3.canceled(() => console.log("I GOT CANCELLED"));
      * p3.cancel(); // will log -----^^
      */
-    class CancelablePromise /* extends Promise */ {
-        constructor(arg) {
+
+    var CancelablePromise /* extends Promise */ = function () {
+        function CancelablePromise(arg) {
+            var _this2 = this;
+
+            _classCallCheck(this, CancelablePromise);
+
             this[IS_CANCELED] = false;
             this[ON_CANCEL] = [];
             if (arg instanceof CancelablePromise) {
@@ -55,10 +89,10 @@
             } else {
                 this[INTERNAL_PROMISE] = Promise.resolve(arg);
             }
-            this[INTERNAL_PROMISE].then(() => {
-                this[IS_RESOLVED] = !this[IS_CANCELED];
-            }, () => {
-                this[IS_REJECTED] = !this[IS_CANCELED];
+            this[INTERNAL_PROMISE].then(function () {
+                _this2[IS_RESOLVED] = !_this2[IS_CANCELED];
+            }, function () {
+                _this2[IS_REJECTED] = !_this2[IS_CANCELED];
             });
         }
 
@@ -70,168 +104,129 @@
          * @param  {Function} [onCanceled]  The callback to execute when the {CancelablePromise} is canceled.
          * @return {CancelablePromise} for the completion of which ever callback is executed.
          */
-        then(onResolved, onRejected, onCanceled) {
-            return CancelablePromise.race([this[INTERNAL_PROMISE].then(fnWrapper.call(this, onResolved), fnWrapper.call(this, onRejected)), onCanceled && this.canceled(onCanceled)]);
-        }
 
-        /**
-         * Attaches a callback for only the rejection of the {CancelablePromise}.
-         *
-         * @param  {Function} onRejected  The callback to execute when the {CancelablePromise} is rejected.
-         * @return {CancelablePromise} for the completion of the callback.
-         */
-        catch(onRejected) {
-            return new CancelablePromise(this[INTERNAL_PROMISE].catch(fnWrapper.call(this, onRejected)));
-        }
 
-        /**
-         * Attaches a callback for any fulfillment of the {CancelablePromise}.
-         *
-         * @param  {Function} fn  The callback to execute when the {CancelablePromise} is fulfilled.
-         * @return {CancelablePromise} for the completion of the callback.
-         */
-        finally(fn) {
-            return this.then(fn, fn, fn);
-        }
-
-        /**
-         * Cancel the {CancelablePromise}
-         */
-        cancel() {
-            if (!this[IS_CANCELED] && !this[IS_RESOLVED] && !this[IS_REJECTED]) {
-                this[IS_CANCELED] = true;
-                while (this[ON_CANCEL].length) {
-                    try {
-                        this[ON_CANCEL].shift()();
-                    } catch (e) {
-                        console.error(e); // eslint-disable-line no-console
+        _createClass(CancelablePromise, [{
+            key: "then",
+            value: function then(onResolved, onRejected, onCanceled) {
+                return CancelablePromise.race([this[INTERNAL_PROMISE].then(fnWrapper.call(this, onResolved), fnWrapper.call(this, onRejected)), onCanceled && this.canceled(onCanceled)]);
+            }
+        }, {
+            key: "catch",
+            value: function _catch(onRejected) {
+                return new CancelablePromise(this[INTERNAL_PROMISE].catch(fnWrapper.call(this, onRejected)));
+            }
+        }, {
+            key: "finally",
+            value: function _finally(fn) {
+                return this.then(fn, fn, fn);
+            }
+        }, {
+            key: "cancel",
+            value: function cancel() {
+                if (!this[IS_CANCELED] && !this[IS_RESOLVED] && !this[IS_REJECTED]) {
+                    this[IS_CANCELED] = true;
+                    while (this[ON_CANCEL].length) {
+                        try {
+                            this[ON_CANCEL].shift()();
+                        } catch (e) {
+                            console.error(e); // eslint-disable-line no-console
+                        }
                     }
                 }
             }
-        }
-
-        /**
-         * Attaches a callback for only the cancelation of the {CancelablePromise}.
-         *
-         * @param  {Function} onCanceled  The callback to execute when the {CancelablePromise} is canceled.
-         * @return {CancelablePromise} for the completion of the callback.
-         */
-        canceled(onCanceled) {
-            if (this[IS_CANCELED] && !this[ON_CANCEL].length) {
-                return CancelablePromise.try(onCanceled);
+        }, {
+            key: "canceled",
+            value: function canceled(onCanceled) {
+                if (this[IS_CANCELED] && !this[ON_CANCEL].length) {
+                    return CancelablePromise.try(onCanceled);
+                }
+                var resolve = void 0;
+                var p = new CancelablePromise(function (r) {
+                    resolve = r;
+                }).then(onCanceled);
+                this[ON_CANCEL].push(resolve);
+                return p;
             }
-            let resolve;
-            const p = new CancelablePromise(r => {
-                resolve = r;
-            }).then(onCanceled);
-            this[ON_CANCEL].push(resolve);
-            return p;
-        }
-
-        /**
-         * Is the {CancelablePromise} canceled?
-         *
-         * @return {Boolean}
-         */
-        isCanceled() {
-            return this[IS_CANCELED];
-        }
-
-        /**
-         * Is the {CancelablePromise} resolved?
-         *
-         * @return {Boolean}
-         */
-        isResolved() {
-            return this[IS_RESOLVED];
-        }
-
-        /**
-         * Is the {CancelablePromise} rejected?
-         *
-         * @return {Boolean}
-         */
-        isRejected() {
-            return this[IS_REJECTED];
-        }
-
-        /**
-         * Creates a {CancelablePromise} that is resolved with an array of results when all
-         * of the provided {Promise}s resolve, or rejected when any {Promise} is
-         * rejected, or canceled when any {CancelablePromise} is canceled.
-         *
-         * @param  {Mixed[]} values  An array of {Promise}s, {CancelablePromise}s, and/or other values.
-         * @return {CancelablePromise.<Mixed[]>}
-         */
-        static all(values = []) {
-            const CancelablePromises = values.map(p => p instanceof Promise ? new CancelablePromise(p) : p);
-            const result = new CancelablePromise(Promise.all(CancelablePromises));
-            // if the parent is cancelled, cancel all of the children
-            result.canceled(() => CancelablePromises.forEach(p => p instanceof CancelablePromise && p.cancel()));
-            // if any child is cancelled, cancel the parent also
-            // (which will then cancel the other children)
-            CancelablePromises.forEach(p => p instanceof CancelablePromise && p.canceled(() => result.cancel()));
-            return result;
-        }
-
-        /**
-         * Cancel a promise
-         *
-         * @param {CancelablePromise} promise
-         */
-        static cancel(promise) {
-            if (promise instanceof CancelablePromise) {
-                promise.cancel();
+        }, {
+            key: "isCanceled",
+            value: function isCanceled() {
+                return this[IS_CANCELED];
             }
-        }
-
-        /**
-         * Creates a {CancelablePromise} that is resolved or rejected when any of the provided
-         * Promises are resolved or rejected.
-         *
-         * @param  {Mixed} values  An array of {Promise}s, {CancelablePromise}s, and/or other values.
-         * @return {CancelablePromise}
-         */
-        static race(values = []) {
-            return new CancelablePromise(Promise.race(values));
-            // NOTE: unlike `all`, `race` doesn't cancel children
-        }
-
-        /**
-         * Creates a new rejected {CancelablePromise} for the provided reason.
-         *
-         * @param  {Error} reason  The reason {CancelablePromise} was rejected.
-         * @return {CancelablePromise}
-         */
-        static reject(reason) {
-            const p = new CancelablePromise(Promise.reject(reason));
-            p[IS_REJECTED] = true;
-            return p;
-        }
-
-        /**
-         * Creates a new resolved {CancelablePromise} for the provided value.
-         *
-         * @param  {Mixed} value
-         * @return {CancelablePromise}
-         */
-        static resolve(value) {
-            const p = new CancelablePromise(Promise.resolve(value));
-            if (!(value instanceof Promise || value instanceof CancelablePromise)) {
-                p[IS_RESOLVED] = true;
+        }, {
+            key: "isResolved",
+            value: function isResolved() {
+                return this[IS_RESOLVED];
             }
-            return p;
-        }
+        }, {
+            key: "isRejected",
+            value: function isRejected() {
+                return this[IS_REJECTED];
+            }
+        }], [{
+            key: "all",
+            value: function all() {
+                var values = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
 
-        /**
-         * Calls the promised function in a future turn.
-         *
-         * @param  {Function} fn
-         * @return {CancelablePromise}
-         */
-        static try(fn) {
-            return Promise.try ? new CancelablePromise(Promise.try(fn)) : new CancelablePromise(Promise.resolve().then(fn));
-        }
-    }
+                var CancelablePromises = values.map(function (p) {
+                    return p instanceof Promise ? new CancelablePromise(p) : p;
+                });
+                var result = new CancelablePromise(Promise.all(CancelablePromises));
+                // if the parent is cancelled, cancel all of the children
+                result.canceled(function () {
+                    return CancelablePromises.forEach(function (p) {
+                        return p instanceof CancelablePromise && p.cancel();
+                    });
+                });
+                // if any child is cancelled, cancel the parent also
+                // (which will then cancel the other children)
+                CancelablePromises.forEach(function (p) {
+                    return p instanceof CancelablePromise && p.canceled(function () {
+                        return result.cancel();
+                    });
+                });
+                return result;
+            }
+        }, {
+            key: "cancel",
+            value: function cancel(promise) {
+                if (promise instanceof CancelablePromise) {
+                    promise.cancel();
+                }
+            }
+        }, {
+            key: "race",
+            value: function race() {
+                var values = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+                return new CancelablePromise(Promise.race(values));
+                // NOTE: unlike `all`, `race` doesn't cancel children
+            }
+        }, {
+            key: "reject",
+            value: function reject(reason) {
+                var p = new CancelablePromise(Promise.reject(reason));
+                p[IS_REJECTED] = true;
+                return p;
+            }
+        }, {
+            key: "resolve",
+            value: function resolve(value) {
+                var p = new CancelablePromise(Promise.resolve(value));
+                if (!(value instanceof Promise || value instanceof CancelablePromise)) {
+                    p[IS_RESOLVED] = true;
+                }
+                return p;
+            }
+        }, {
+            key: "try",
+            value: function _try(fn) {
+                return Promise.try ? new CancelablePromise(Promise.try(fn)) : new CancelablePromise(Promise.resolve().then(fn));
+            }
+        }]);
+
+        return CancelablePromise;
+    }();
+
     exports.default = CancelablePromise;
 });
